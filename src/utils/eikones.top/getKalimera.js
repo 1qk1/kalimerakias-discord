b2040447656d91b8b424207286fcb5b28303f74a
Panagiotis Skarlas <panossk2@gmail.com>
2025-07-18 21:34:20 +0300

    update project

--- src/utils/eikones.top/getKalimera.js
+++ src/utils/eikones.top/getKalimera.js
@@ -7,9 +7,9 @@ const getKalimera = async () => {
   const content = await axios.get("https://eikones.top/category/%ce%b5%ce%b9%ce%ba%cf%8c%ce%bd%ce%b5%cf%82-%cf%84%ce%bf%cf%80-%ce%b3%ce%b9%ce%b1-%ce%ba%ce%b1%ce%bb%ce%b7%ce%bc%ce%ad%cf%81%ce%b1/")
   const $ = cheerio.load(content.data)
   const articles = $("#main article").map((i, el) => {
-    const image = $(el).find(".archive-thumb img").attr("src")
-    const title = $(el).find(".archive-content .entry-title").text()
-    const url = $(el).find(".archive-content .entry-title a").attr("href")
+    const image = $(el).find(".thumbnail img").attr("src")
+    const title = $(el).find(".entry-title").text()
+    const url = $(el).find(".entry-title a").attr("href")
     return {
       image,
       title,
